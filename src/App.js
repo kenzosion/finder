343cb11acc8b3df4b97c5e2f9f57d12cabb77bf3
Haur Kang <haurkang@Haurs-MacBook-Pro.local>
2019-10-24 12:28:53 +0100

    styled navbar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <Navbar />
+       <Navbar title="Github finder" icon="fab fa-github"/>
       </div>
     );
   }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 
 export class Navbar extends Component {
+    static defaultProps = {
+        title: 'finder',
+        icon: "fab fa-github"
+    }
+
     render() {
+    const {title, icon} = this.props;
         return (
-            <div>
-                <h1>Navbar</h1>
-            </div>
+            <nav className="navbar bg-primary">
+                <h1>
+                    <i className={icon} />
+                    {title}
+                </h1>
+            </nav>
         )
     }
 }
