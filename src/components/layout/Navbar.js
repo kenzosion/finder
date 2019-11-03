65d5f77338e05f93dc3dfb6e614f516e297ee224
Haur Kang <haur.kang@gmail.com>
2019-11-03 20:40:22 +0000

    complete router

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 const Navbar = ({title, icon}) => {
         return (
-            <nav className="navbar bg-primary">
+            <nav className="navbar bg-dark">
                 <h1>
                 <i className={icon} style={{marginRight:'1rem'}} />
                 {title}
                 </h1>
+                <ul>
+                    <li>
+                        <Link to="/"> Home </Link>
+                    </li>
+                    <li>
+                        <Link to="/about"> About </Link>
+                    </li>
+                </ul>
             </nav>
         )
     }
