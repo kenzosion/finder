279ae08f720dd1b8829c4a73a68de8e80c3b80dd
Haur Kang <haur.kang@gmail.com>
2019-11-14 22:33:51 +0000

    refactoring to use Hooks

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,50 +1,43 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
 
-class Search extends Component {
-    state = {
-        text: ''
+const Search = ({ showClear, clearUsers, setAlert, searchUsers }) => {
+    const [text, setText] = useState('');
+
+    const onChange = (event) => {
+        setText(event.target.value)
     }
 
-    onChange = (event) => {
-        this.setState({ [event.target.name]: event.target.value})
-    }
-
-    onSubmit = e => {
+    const onSubmit = e => {
         e.preventDefault()
         // alert users to enter some text
-        if (this.state.text === ''){
-            this.props.setAlert('Please enter a user name', 'light')
+        if (text === '') {
+            setAlert('Please enter a user name', 'light')
         } else {
-            this.props.searchUsers(this.state.text)
-            this.setState({ text: ''})
+            searchUsers(text)
+            setText('')
         }
     }
-
-    
-    render() {
-        const {text} = this.state;
-        const {showClear, clearUsers} = this.props;
-        return (
-            <div>
-                <form onSubmit={this.onSubmit} className="form">
-                    <input 
-                        type="text" 
-                        name="text" 
-                        placeholder="Search Github users..." 
-                        value={text}
-                        onChange={this.onChange}/>
-                    <span className="grid-2">
-                    <input type="submit" value="Search" className="btn btn-dark btn-block"/>
+    return (
+        <div>
+            <form onSubmit={onSubmit} className="form">
+                <input
+                    type="text"
+                    name="text"
+                    placeholder="Search Github users..."
+                    value={text}
+                    onChange={onChange} />
+                <span className="grid-2">
+                    <input type="submit" value="Search" className="btn btn-dark btn-block" />
                     {showClear && <input
                         className="btn btn-dark btn-block text-center"
                         onClick={clearUsers}
-                        value="Clear"/>}
-                    </span>
-                </form>
-            </div>
-        )
-    }
+                        value="Clear" />}
+                </span>
+            </form>
+        </div>
+    )
 }
+
 
 export default Search
