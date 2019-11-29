c1489269415fa9fd06c2a7a7cef7c508d84cdf73
Haur Kang <haur.kang@gmail.com>
2019-11-29 23:31:29 +0000

    github reducer

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -5,3 +5,16 @@ import {
     GET_REPOS,
     SET_LOADING
 } from '../types'
+
+export default (state, action) => {
+    switch(action.type) {
+        case SET_LOADING: 
+            return {
+                ...state, 
+                loading: true
+            }
+
+        default: 
+            return state;
+    }
+}
